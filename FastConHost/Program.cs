ae02088434bf014c4f674aa09f71ed7f68916dd7
libichong@hotmail.com <wxr8738078>
2017-02-21 23:30:58 +0800

    a new iteration

--- FastConHost/Program.cs
+++ FastConHost/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
 
 namespace FastConHost
 {
@@ -10,6 +12,21 @@ namespace FastConHost
     {
         static void Main(string[] args)
         {
+            var sw = Stopwatch.StartNew();
+            var files = (new DriveInfo(@"c:\")).EnumerateFiles().ToArray();
+            var elapsed = sw.ElapsedMilliseconds.ToString();
+            Console.WriteLine(string.Format("Found {0} files, elapsed {1} ms", files.Length, elapsed));
+
+            //WordBreaker wordBreaker = new WordBreaker(@"C:\FastCon\Library\words.dll");
+            //foreach(var line in Utility.EnumerateFiles("C:\\Windows"))
+            //{
+            //    Console.WriteLine(line);
+            //}
+
+            //Utility.PathCollector collectorCallBack = new Utility.PathCollector();
+            //USNHelper helper = new USNHelper("C:\\Windows", "C:\\");
+
+            //helper.GetFileSystemEntries("C:\\Windows");
         }
     }
 }
---
+++ FastConHost/WordBreaker.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FastConHost
+{
+    public class WordBreaker
+    {
+        private static bool fInitialized = false;
+        private string DictionartPath = string.Empty;
+        private HashSet<string> DictionarySet = new HashSet<string>();
+        private const string InvalidPathCharcter = "\\/:*?\"<>|";
+        public WordBreaker(string dictionaryFile)
+        {
+            this.DictionartPath = dictionaryFile;
+            if(File.Exists(dictionaryFile))
+            {
+                fInitialized = LoadDictionary(dictionaryFile);
+            }
+        }
+
+        private bool IsValidPath(string word)
+        {
+            for (int i = 0; i < InvalidPathCharcter.Length; i++)
+            {
+                if (word.Contains(InvalidPathCharcter[i].ToString()))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private bool LoadDictionary(string dictionaryFile)
+        {
+            try
+            {
+                FileStream aFile = new FileStream(dictionaryFile, FileMode.Open);
+                StreamReader sr = new StreamReader(aFile);
+                string strLine = sr.ReadLine();
+                while (strLine != null)
+                {
+                    strLine = sr.ReadLine();
+                    if (!string.IsNullOrEmpty(strLine) && IsValidPath(strLine) && strLine.Length > 1)
+                    {
+                        DictionarySet.Add(strLine);
+                    }
+                }
+                sr.Close();
+            }
+            catch (IOException ex)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
